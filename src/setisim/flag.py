462f38ff88fdc1f17fdfc6a34059f13111c36d7d
Avinash Kumar <avialxee@gmail.com>
2022-06-20 22:25:46 -0700

    return flagged vis

--- src/setisim/flag.py
+++ src/setisim/flag.py
@@ -1,9 +1,20 @@
 import os
 from pathlib import Path
-from setisim import split, plotd, init_flag, flagsummary, flagdata, gaincal, applycal
+from setisim import split, plotd, init_flag, flagsummary, flagdata, gaincal, applycal, listobs
 from setisim.calib import solint_p
 
 def flagger(vis, **kwargs):
+    """
+    Does automated flagging of bad channel and bad antenna;
+
+    Returns
+    -------
+    Flagged visiblity file.
+
+    TODO: 
+    1. For badant flagging employing solution from gaincal in 'p' mode
+    should have maximum solution interval?
+    """
     visd={
         'f0':Path(vis).stem+'f0.MS',
         'f1':Path(vis).stem+'f1.MS',
@@ -16,11 +27,15 @@ def flagger(vis, **kwargs):
     params.update(kwargs)
     os.system(f"rm -rf {'* '.join(visd.values())}")
 
+    metadata=listobs(vis, verbose=True)
+    scanl = {k.replace('scan_', ''):v['0']['FieldName'] for k,v in metadata.items() if 'scan_' in k}
+    scanlist = scanl.keys()
+
     m ="""# quacking, shadowing, clipping"""
     print(m)
     if params['plots']: plotd(vis)
     split(vis,visd['f0'], datacolumn='data')
-    for scan in ['1','2','3']:
+    for scan in scanlist:
         init_flag(visd['f0'], quackmode='endb', scan=scan)
         init_flag(visd['f0'], quackmode='beg', scan=scan)
     if params['flagsummary']:flagsummary(visd['f0'])
@@ -29,7 +44,7 @@ def flagger(vis, **kwargs):
     m ="""# tfcrop independently on each calibrators and target"""
     print(m)
     split(visd['f0'],visd['f1'], datacolumn='data')
-    for scan in ['1','2','3']:
+    for scan in scanlist:
         flagdata(vis=visd['f1'], mode='tfcrop', spw='0', datacolumn='data', action='apply', scan=scan,
                     flagbackup=True)
         init_flag(visd['f1'], extend=True)
@@ -57,8 +72,9 @@ def flagger(vis, **kwargs):
     print(f'{m}:{badant}')
     
     split(visd['f1c0c'],visd['f2'], datacolumn='data')
-    for scan in ['1','2','3']:
+    for scan in scanlist:
         init_flag(visd['f2'], badant=badant, extend=True, scan=scan)
     if params['flagsummary']:flagsummary(visd['f2'])
     if params['plots']:plotd(visd['f2'])
     print(f'flagged data:{visd["f2"]}')
+    return visd['f2']
